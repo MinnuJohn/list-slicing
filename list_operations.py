9cd55511b61c7a318b08e5084e394d2f8696b82c
Minnu John <minnu.john@snhu.edu>
2023-05-22 13:52:11 -0500

    Fuctions top and first three complected

--- list_operations.py
+++ list_operations.py
@@ -51,7 +51,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 def first_three(input_list):
@@ -64,7 +64,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
